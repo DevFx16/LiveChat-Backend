20968436f36ad06322e111dd9822c671593e101b
Fernando Araujo <fernandoaraujo081@gmail.com>
2018-09-18 22:47:36 -0500

    Credencial

--- Controllers/Firebase.js
+++ Controllers/Firebase.js
@@ -1,15 +1,13 @@
 import * as admin from 'firebase-admin';
 import * as firebase from 'firebase';
+import Cred from '../Credencial/CredencialAuth';
 
 var Admin = require('../Credencial/Credencial');
 
 //Inicializo mi app de firebase
 admin.initializeApp({ credential: admin.credential.cert(Admin) });
 
-firebase.initializeApp({
-    apiKey: "AIzaSyBxlTgDg99WfICIkX4TqiccNTqfndCnjoY",
-    authDomain: "livechat-6e105.firebaseapp.com",
-});
+firebase.initializeApp(Cred);
 
 export const Auth = firebase;
 export default admin;
